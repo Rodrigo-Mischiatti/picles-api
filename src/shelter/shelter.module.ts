87a0c26c9b6a99ed9708f3d9347741e83153061d
Rodrigo-Mischiatti <rodrigomisc@hotmail.com>
2024-04-04 22:33:33 -0300

    criando conexao com banco de dados Mongo

--- src/shelter/shelter.module.ts
+++ src/shelter/shelter.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ShelterController } from './shelter.controller';
 import ShelterTokens from './shelter.tokens';
 import GetShelterDetailsUseCase from './usecases/get.shelter.details.usecases';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Shelter, ShelterSchema } from './schemas/shelter.schema';
 
 @Module({
   controllers: [ShelterController],
+  imports: [MongooseModule.forFeature([{ name: Shelter.name, schema: ShelterSchema }])],
   providers: [
     {
       provide: ShelterTokens.getShelterDetailsUseCase,
